974fb18ddcebc4a07b2d05deb24c762f40265f8d
ElLuizon <luis.perezma18@tectijuana.edu.mx>
2025-03-17 16:13:28 -0700

    Adding a New Grid button

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 // Seleccionar el contenedor
 const container = document.querySelector("#container");
 
-// Tamaño de la cuadricula (16x16)
-const gridSize = 16;
+// Seleccionar el boton
+const newGrid = document.querySelector("#newGrid");
 
-//crear la cuadricula con ciclo for
-for(let i = 0; i < gridSize * gridSize; i++){
-    //crear un nuevo elemento div
-    const square = document.createElement("div");
+// Funcion para crear la cuadricula
+function createGrid(gridSize) {
+    //limpiar el contenedor
+    container.innerHTML = "";
+    
+    // calcular el tamaño del cuadrado
+    const squareSize = 960/ gridSize; //960px es el tamaño total de la cuadricula
 
-    //Agregar la clase "square" al div
-    square.classList.add("square");
+    
+    //crear la cuadricula con ciclo for
+    for(let i = 0; i < gridSize * gridSize; i++){
+        //crear un nuevo elemento div
+        const square = document.createElement("div");
+    
+        //Agregar la clase "square" al div
+        square.classList.add("square");
+        
+        // Establecer el tamaño del cuadrado 
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
-    // Agregar el div al contenedor
-    container.appendChild(square)
+        //Agregar el efecto de 'Hover´para cuando pase el raton en el cuadro
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";//cabmia el fondo a negro
+        });
+        // Agregar el div al contenedor
+        container.appendChild(square);
+    
+    }
 }
+
+// Crear una cuadricula inicial
+createGrid(16);
+
+// Agregar un event listener al boton
+newGrid.addEventListener("click", () => {
+    // Solicitar al usuario el numero de cuadros por lado
+    const newGridSize = prompt("Add the number of squares by side (maximun 100)");
+
+    // Validar la entrada del usuario
+    if (newGridSize && !isNaN(newGridSize)) {
+        const size = parseInt(newGridSize);
+
+        if(size > 0 && size <= 100) {
+            //crear una nueva cuadricula con el tamaño especificado
+            createGrid(size);
+        } else {
+            alert("please, add a number between 1 & 100");
+        }
+    } else {
+        alert("Invalid Input. Please add a number");
+    }
+});
